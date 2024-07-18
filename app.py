adc5df8e471817ebf3c70bfeae80b9681540ab5c
Kamesh <kamesh.iitbombay@gmail.com>
2024-07-17 19:10:30 -0700

    home-page added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    return render_template("index.html")
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
